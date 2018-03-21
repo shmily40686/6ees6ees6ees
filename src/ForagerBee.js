31bd38f4af4b08bab02ec5f9b2f3ac310253c154
Frog <shmily40686@gmail.com>
2018-03-21 11:22:17 -0700

    finshed ES6 style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor(){
+  	super();
+  	this.age = 10 ;
+  	this.canFly = true;
+  	this.job = 'find pollen'
+  	this.treasureChest = [];
+  }
+  forage(){
+  	this.treasureChest.push('treasure');
+  }
 };
